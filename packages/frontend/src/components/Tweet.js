a94695668da21c18743297b231556004534c705e
Leonardo Pessoa <nardopessoa@gmail.com>
2018-11-10 02:35:29 -0200

    Impl ação de like de um tweet

--- packages/frontend/src/components/Tweet.js
+++ packages/frontend/src/components/Tweet.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import "./Tweet.css";
 import like from "../like.svg";
+import api from "../services/api";
 
 export default class Tweet extends Component {
+  handleLike = async () => {
+    const { _id } = this.props.tweet;
+
+    await api.post(`likes/${_id}`);
+  };
+
   render() {
     const { tweet } = this.props;
 
@@ -10,7 +17,7 @@ export default class Tweet extends Component {
       <li className="tweet">
         <strong>{tweet.author}</strong>
         <p>{tweet.content}</p>
-        <button type="button">
+        <button type="button" onClick={this.handleLike}>
           <img src={like} alt="Like" />
           {tweet.likes}
         </button>
