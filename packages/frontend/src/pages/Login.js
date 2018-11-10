8cbd86450ded0a17f6d6ec4230f1c9ed39b4547e
Leonardo Pessoa <nardopessoa@gmail.com>
2018-11-10 02:03:11 -0200

    Impl tela login, armazena usuario no localstorage e navega p/ timeline

--- packages/frontend/src/pages/Login.js
+++ packages/frontend/src/pages/Login.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+import "./Login.css";
+import twitterLogo from "../twitter.svg";
 
 export default class Login extends Component {
+  state = {
+    username: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem("@GoTwitter:username", username);
+
+    this.props.history.push("/timeline");
+  };
+
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogo} alt="GoTwitter" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Nome de usuário"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+          />
+          <button>Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
