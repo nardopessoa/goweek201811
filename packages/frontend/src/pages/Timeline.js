eaee5e0c65cdbf33a2222d7397a7da7b966a1b33
Leonardo Pessoa <nardopessoa@gmail.com>
2018-11-10 02:15:38 -0200

    Impl formulário de inserção de novo tweet na timeline

--- packages/frontend/src/pages/Timeline.js
+++ packages/frontend/src/pages/Timeline.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import "./Timeline.css";
+import twitterLogo from "../twitter.svg";
 
 export default class Timeline extends Component {
+  state = {
+    newTweet: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({ newTweet: event.target.value });
+  };
+
+  handleNewTweet = event => {
+    if (event.keyCode !== 13) return;
+
+    const content = this.state.newTweet;
+    const author = localStorage.getItem("@GoTwitter:username");
+    console.log(content, author);
+  };
+
   render() {
-    return <h1>Timeline</h1>;
+    return (
+      <div className="timeline-wrapper">
+        <img src={twitterLogo} alt="GoTwitter" height={24} />
+
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que esta acontecendo?"
+          />
+        </form>
+      </div>
+    );
   }
 }
