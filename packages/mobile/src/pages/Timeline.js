7f0b1a8ff77ca6ae674779084100b459ee8125e2
Leonardo Pessoa <nardopessoa@gmail.com>
2018-11-11 02:44:02 -0200

    Impl funções de atualização em tempo real

--- packages/mobile/src/pages/Timeline.js
+++ packages/mobile/src/pages/Timeline.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, TouchableOpacity, FlatList } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import api from "../services/api";
 import Tweet from "../components/Tweet";
+import socket from "socket.io-client";
 
 export default class Timeline extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -24,10 +25,27 @@ export default class Timeline extends Component {
   };
 
   async componentDidMount() {
+    this.subscribeToEvents();
+
     const response = await api.get("/tweets");
 
     this.setState({ tweets: response.data });
   }
+
+  subscribeToEvents = () => {
+    const io = socket("http://10.0.3.2:3000");
+
+    io.on("tweet", data => {
+      this.setState(state => ({ tweets: [data, ...state.tweets] }));
+    });
+    io.on("like", data => {
+      this.setState(state => ({
+        tweets: state.tweets.map(tweet =>
+          data._id === tweet._id ? data : tweet
+        )
+      }));
+    });
+  };
 
   render() {
     return (
